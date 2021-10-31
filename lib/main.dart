e36683683b900ef5e38b9c8b54472eba9f5f85c5
hrishikeshpanhalkar <hrishikeshpanhalkar7@gmail.com>
2021-10-31 21:51:58 +0530

    added post request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:httptutorial/services/post_service.dart';
 
 void main() {
@@ -60,36 +61,107 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: FutureBuilder<List>(
-        future: postService.getPosts(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data?.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(snapshot.data?[index]['title']),
-                    subtitle: Text(snapshot.data?[index]['body']),
-                  );
-                });
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(
-                color: Colors.black,
-              ),
-            );
-          }
-        },
-      ),
-    );
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder<List>(
+          future: postService.getPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(snapshot.data?[index]['title']),
+                      subtitle: Text(snapshot.data?[index]['body']),
+                    );
+                  });
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+              );
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          var userId;
+          String? title;
+          String? body;
+          showDialog(
+              barrierDismissible: false,
+              context: context,
+              builder: (context) => AlertDialog(
+                    content: Form(
+                      child: Column(mainAxisSize: MainAxisSize.min, children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            labelText: "UserID",
+                          ),
+                          onChanged: (_val) {
+                            userId = _val;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            labelText: "Title",
+                          ),
+                          onChanged: (_val) {
+                            title = _val;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            labelText: "Body",
+                          ),
+                          onChanged: (_val) {
+                            body = _val;
+                          },
+                        ),
+                        SizedBox(
+                          height: 12.0,
+                        ),
+                        ElevatedButton(
+                            onPressed: () async {
+                              Map<String, dynamic> data = {
+                                "title": title,
+                                "body": body,
+                              };
+                              String res = await postService.createPost(data);
+                              res == "success"
+                                  ? Fluttertoast.showToast(
+                                      msg: "Post Created!!")
+                                  : Fluttertoast.showToast(
+                                      msg: "Error Creating Post !!");
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              "Add",
+                            ))
+                      ]),
+                    ),
+                  ));
+        }));
   }
 }
+
+// {
+//     "userId": 1,
+//     "title": "hrishikesh",
+//     "body": "Panhalkar"
+// }
+
+// http://jsonplaceholder.typicode.com/posts
--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PostService {
-  String baseUrl = "https://jsonplaceholder.typicode.com/posts";
+  String baseUrl = "http://jsonplaceholder.typicode.com/posts";
 
   // returns a list of all posts as per my API
   // may use this in a futurebuilder
@@ -12,7 +12,7 @@ class PostService {
       var response = await http.get(Uri.parse(baseUrl));
       if (response.statusCode == 200) {
         // return a decoded body
-        print(response.body);
+        //print(response.body);
         return jsonDecode(response.body);
       } else {
         // server error
@@ -22,6 +22,28 @@ class PostService {
       // fetching error
       // may be timeout, no internet or dns not resolved
       return Future.error("Error Fetching Data !");
+    }
+  }
+
+  Future<String> createPost(Map<String, dynamic> data) async {
+    try {
+      var response =
+          await http.post(Uri.parse(baseUrl), body: jsonEncode(data), headers: {
+        'Content-Type': 'application/json',
+      });
+      if (response.statusCode == 201) {
+        // return a decoded body
+        print(response.body);
+        return "success";
+      } else {
+        // server error
+        print(response.body);
+        return "err";
+      }
+    } catch (socketException) {
+      // fetching error
+      // may be timeout, no internet or dns not resolved
+      return "err";
     }
   }
 }
